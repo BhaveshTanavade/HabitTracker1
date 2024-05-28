b80ab43a778b65acbc71ac66816657d5edf231d9
BhaveshTanavade <bhavtanavade1@gmail.com>
2024-05-28 21:22:34 +0530

    commiit

--- config/db.js
+++ config/db.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 export const connectToDb = async()=>{
   try{
     console.log("Connecting to DB....");
-    const resp = await mongoose.connect("mongodb://localhost:27017/habitTracker");
+    const resp = await mongoose.connect("mongodb://35.160.120.126:27017/habitTracker");
     console.log("Connected to mongoose DB");
   }catch(err){
     console.log("mongodb connection failed!");
