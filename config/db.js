8d7ad17a8b6f7ba974189857a3f65d3caead1b39
BhaveshTanavade <bhavtanavade1@gmail.com>
2024-05-28 21:39:26 +0530

    update db.js

--- config/db.js
+++ config/db.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 export const connectToDb = async()=>{
   try{
     console.log("Connecting to DB....");
-    const resp = await mongoose.connect("mongodb://35.160.120.126:27017/habitTracker");
+    const resp = await mongoose.connect("mongodb+srv://Bhavesh:Password1@cluster0.hgfwvey.mongodb.net/?retryWrites=true&w=majority&appName=HabitTrackernode");
     console.log("Connected to mongoose DB");
   }catch(err){
     console.log("mongodb connection failed!");
